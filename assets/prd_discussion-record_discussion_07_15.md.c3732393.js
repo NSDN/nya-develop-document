15f90de7b8e648f4d2695f9429791cfc1d1b0a72
SetsuikiHyoryu <SetsuikiHyoryu@users.noreply.github.com>
2022-12-24 01:06:36 +0000

    deploy: 6796ac91421a45af34b3a7fb85840ee250aec8a0

---
+++ assets/prd_discussion-record_discussion_07_15.md.c3732393.js
@@ -0,0 +1,19 @@
+import{_ as s,c as n,o as a,a as e}from"./app.6f248088.js";const m=JSON.parse('{"title":"2022.07.15 需求讨论（中午）","description":"","frontmatter":{},"headers":[],"relativePath":"prd/discussion-record/discussion_07_15.md","lastUpdated":1671843863000}'),l={name:"prd/discussion-record/discussion_07_15.md"},p=e(`<h1 id="_2022-07-15-需求讨论-中午" tabindex="-1">2022.07.15 需求讨论（中午） <a class="header-anchor" href="#_2022-07-15-需求讨论-中午" aria-hidden="true">#</a></h1><div class="language-txt line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">txt</span><pre class="shiki material-palenight"><code><span class="line"><span style="color:#A6ACCD;">一个thread（主贴）里面有一级回复message（帖子），然后message上面还有附属的二级回复 sub message（点评）。之前遇到的问题是参与点评的人在点评被回复后无法收到消息提醒</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">富文本编辑器 VS MD编辑器</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">用户信息panel</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">共同创作标签</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">点评形式</span></span>
+<span class="line"><span style="color:#A6ACCD;">Thread</span></span>
+<span class="line"><span style="color:#A6ACCD;">	- message</span></span>
+<span class="line"><span style="color:#A6ACCD;">		- sub message</span></span>
+<span class="line"><span style="color:#A6ACCD;">NGA 形式</span></span>
+<span class="line"><span style="color:#A6ACCD;">Thread</span></span>
+<span class="line"><span style="color:#A6ACCD;">	- message1(link to message2)</span></span>
+<span class="line"><span style="color:#A6ACCD;">	- message2(link to message1)</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">骰子功能（给定范围和次数的随机数生成）</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br></div></div>`,2),r=[p];function c(i,o,t,b,A,C){return a(),n("div",null,r)}const u=s(l,[["render",c]]);export{m as __pageData,u as default};
