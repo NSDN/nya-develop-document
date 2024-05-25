eb7edaa4f031ba716af95fa588c203eb22c7e204
SetsuikiHyoryu <SetsuikiHyoryu@users.noreply.github.com>
2024-05-25 09:43:39 +0000

    deploy: 526cfe41a83549e6ff9b4444e51670a689bd6f95

---
+++ assets/prd_discussion-record_discussion_07_15.md.CgIgeUgT.js
@@ -0,0 +1,18 @@
+import{_ as s,c as n,o as a,a1 as e}from"./chunks/framework.B3uPhfhi.js";const _=JSON.parse('{"title":"2022.07.15 需求讨论（中午）","description":"","frontmatter":{},"headers":[],"relativePath":"prd/discussion-record/discussion_07_15.md","filePath":"prd/discussion-record/discussion_07_15.md","lastUpdated":1671843863000}'),p={name:"prd/discussion-record/discussion_07_15.md"},l=e(`<h1 id="_2022-07-15-需求讨论-中午" tabindex="-1">2022.07.15 需求讨论（中午） <a class="header-anchor" href="#_2022-07-15-需求讨论-中午" aria-label="Permalink to &quot;2022.07.15 需求讨论（中午）&quot;">​</a></h1><div class="language-txt vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">txt</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>一个thread（主贴）里面有一级回复message（帖子），然后message上面还有附属的二级回复 sub message（点评）。之前遇到的问题是参与点评的人在点评被回复后无法收到消息提醒</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>富文本编辑器 VS MD编辑器</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>用户信息panel</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>共同创作标签</span></span>
+<span class="line"><span> </span></span>
+<span class="line"><span>点评形式</span></span>
+<span class="line"><span>Thread</span></span>
+<span class="line"><span>	- message</span></span>
+<span class="line"><span>		- sub message</span></span>
+<span class="line"><span>NGA 形式</span></span>
+<span class="line"><span>Thread</span></span>
+<span class="line"><span>	- message1(link to message2)</span></span>
+<span class="line"><span>	- message2(link to message1)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>骰子功能（给定范围和次数的随机数生成）</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br></div></div>`,2),i=[l];function r(c,t,d,b,o,m){return a(),n("div",null,i)}const h=s(p,[["render",r]]);export{_ as __pageData,h as default};
